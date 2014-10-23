eb5d3b8ff36c59f6cdecadfe244cef70108d552f
Michał Šrajer <michal.srajer@mobica.com>
2014-10-23 22:25:36 +0200

    ver 0.0.2.0 - giveup button

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
       cell.classList.toggle('one');
       cell.classList.toggle('zero');
       doesItMatch()
-    });
+    }, true);
   });
 
   function doesItMatch() {
@@ -34,6 +34,18 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
+  function giveup() {
+    if (mode === MODE_LOOK) {
+      return;
+    }
+    console.log("giveup");
+    Array.prototype.forEach.call(cells, function(cell, i) {
+      if (window.shown[i] === 1) {
+        cell.classList.toggle('hint');
+      }
+    });
+  }
+
   function startGuessing() {
     Array.prototype.forEach.call(cells, function(cell, i) {
       window.guess[i] = 0;
@@ -41,13 +53,20 @@ document.addEventListener('DOMContentLoaded', function() {
       cell.classList.add('zero');
     });
     mode = MODE_GUESS;
+    msg("Reconstruct bits.");
+  }
+
+  function msg(text) {
+    game.querySelector('.controls .message').innerText = text;
   }
 
   function initialize() {
     mode = MODE_LOOK;
+    msg("Try to remember bits amd click the board.");
     Array.prototype.forEach.call(cells, function(cell, i) {
       cell.setAttribute("board_cell", i);
       window.guess[i] = 0;
+      cell.classList.remove('hint');
       if (Math.random() < 0.4) {
         window.shown[i] = 0;
         cell.classList.remove('one');
@@ -64,6 +83,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.querySelector('#restart').addEventListener('click', function() {
     initialize();
+  });
+
+  document.querySelector('#giveup').addEventListener('click', function() {
+    giveup();
   });
 
 });
