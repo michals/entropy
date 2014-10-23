5fbfdab15d3fd897122fa83180998305afc45c30
Michał Šrajer <michal.srajer@mobica.com>
2014-10-23 10:59:27 +0200

    first playable version

--- js/main.js
+++ js/main.js
@@ -5,33 +5,65 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // window.board = new Board();
   var SIZE = 5;
-  window.board = new Array(SIZE*SIZE);
+  window.shown = new Array(SIZE*SIZE);
+  window.guess = new Array(SIZE*SIZE);
 
-  function generate() {
+  var MODE_LOOK = "look", MODE_GUESS = "GUESS", mode = MODE_LOOK;
+
+  Array.prototype.forEach.call(cells, function(cell, i) {
+    cell.addEventListener('click', function(event) {
+      if (mode === MODE_LOOK) {
+        startGuessing();
+        return;
+      }
+      window.guess[i] = (window.guess[i]) ? 0 : 1;
+      cell.classList.toggle('one');
+      cell.classList.toggle('zero');
+      doesItMatch()
+    });
+  });
+
+  function doesItMatch() {
+    var shownStr = window.shown.join(''),
+        guessStr = window.guess.join('');
+    console.log(shownStr);
+    console.log(guessStr);
+    if (shownStr === guessStr) {
+      console.log("good job!");
+      initialize();
+    }
+  }
+
+  function startGuessing() {
+    Array.prototype.forEach.call(cells, function(cell, i) {
+      window.guess[i] = 0;
+      cell.classList.remove('one');
+      cell.classList.add('zero');
+    });
+    mode = MODE_GUESS;
+  }
+
+  function initialize() {
+    mode = MODE_LOOK;
     Array.prototype.forEach.call(cells, function(cell, i) {
       cell.setAttribute("board_cell", i);
+      window.guess[i] = 0;
       if (Math.random() < 0.4) {
-        window.board[i] = 0;
+        window.shown[i] = 0;
         cell.classList.remove('one');
         cell.classList.add('zero');
       } else {
-        window.board[i] = 1;
+        window.shown[i] = 1;
         cell.classList.remove('zero');
         cell.classList.add('one');
       }
-      cell.addEventListener('click', function(event) {
-        console.log('click ' + i);
-        window.board[i] = (window.board[i]) ? 0 : 1;
-        cell.classList.toggle('one');
-        cell.classList.toggle('zero');
-      });
     });
   }
 
-  generate();
+  initialize();
 
   document.querySelector('#restart').addEventListener('click', function() {
-    generate();
+    initialize();
   });
 
 });
